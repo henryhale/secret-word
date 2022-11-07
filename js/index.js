9fccdf71356b2b1ea0057c32bd79c973b47a0ee9
henryhale <henryhale@users.noreply.github.com>
2022-11-07 13:10:20 -0500

    updated

--- js/index.js
+++ js/index.js
@@ -1,16 +1,35 @@
 import { queryEl } from "./helpers.js";
 import { switchViewTo } from "./views.js";
+import game from "./model/index.js";
+import { checkMode, loadImages, toggleDarkMode } from "./plugins.js";
 
+// DOM elements reference
+const [ startBtn, gameInputs, gameHint, gameCheckBtn, darkModeBtn, gameGif ] = [
+  '#startBtn',
+  '#gameInputs',
+  '#gameHint',
+  '#gameCheckBtn',
+  '#darkModeBtn',
+  '#gameGif'
+].map((el) => queryEl(el));
 
 // Start Button
-const startBtn = queryEl('#startBtn');
 startBtn.addEventListener('click', function () {
   // New game
   switchViewTo('game');
 });
 
+// Set output elements
+game.setOutputs(gameInputs, gameHint);
 
+// Whenever user checks
+gameCheckBtn?.addEventListener("click", game.checkAll);
 
+// Dark Mode
+darkModeBtn?.addEventListener("click", e => toggleDarkMode(document.documentElement, darkModeBtn));
+
+// Gif mount element
+game.setReaction(gameGif);
 
 // Finally...
 window.addEventListener(
@@ -18,6 +37,13 @@ window.addEventListener(
     function () {
       // Hide loader & switch to start
       switchViewTo('start');
+      // check theme from local storage
+      checkMode(darkModeBtn);
     },
     false
 );
+
+// Start...
+// load gif images, and then start...
+loadImages(() => game.startGame());
+  
